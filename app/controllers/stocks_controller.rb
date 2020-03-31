4dbf35b0042ec010b184ecb74bb31e55a1b02f72
Linal180 <amirnisar75@gmail.com>
2020-04-01 00:40:12 +0500

    Add fisrt and last name in signup

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,25 @@
+class StocksController < ApplicationController
+  
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js {render partial: 'users/result'} #result.js in users controller
+        end
+      else
+        respond_to do |format| 
+          flash.now[:alert] = 'Please enter a valid symbol'
+          format.js {render partial: 'users/result'}
+        end  
+      end
+    else
+      respond_to do |format| 
+        flash.now[:alert] = 'Please enter a symbol to search'
+        format.js {render partial: 'users/result'}
+      end  
+    end
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
   get 'about', to: 'welcome#about'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
+
 end
