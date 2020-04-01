041f11539633aaa61989ca4d12e30ec7242e25b2
Linal180 <amirnisar75@gmail.com>
2020-04-01 23:43:44 +0500

    remove friend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,29 @@ class UsersController < ApplicationController
   end
 
   def my_friends
-    @friends = current_user.friends 
+    @my_friends = current_user.friends 
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends  )
+      if @friends
+        respond_to do |format|
+          format.js {render partial: 'friends/result'} #result.js in users view
+        end
+      else
+          respond_to do |format| 
+            flash.now[:alert] = "No friend found with Name: #{params[:friend]}"
+            format.js {render partial: 'friends/result'}
+          end  
+      end
+    else
+        respond_to do |format| 
+          flash.now[:alert] = 'Please enter a Name to search'
+          format.js {render partial: 'friends/result'}
+        end  
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'friendship/create'
+  get 'friendship/destroy'
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
   get 'about', to: 'welcome#about'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
-
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  resources :friendship, only: [:create, :destroy]
 end
